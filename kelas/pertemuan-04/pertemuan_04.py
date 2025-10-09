9f7edd580142b09f494f9e3ac10a7b4bb6dbbb56
rae <rasyadanafdhalf@gmail.com>
2025-10-09 19:37:45 +0800

    add_pertemuan-4

---
+++ kelas/pertemuan-04/pertemuan_04.py
@@ -0,0 +1,44 @@
+# loop
+# ngawi = 10
+# for i in range (ngawi):
+#  print(f'Perulangan ke {i + 11}')
+
+# step
+# for i in range (2,10,3):
+#  print(f'Perulangan ke {i}')
+
+# for i in range(1,5):
+#     for j in range(5,11 ):
+#         print(f'{i} x {j} = {i * j}')
+#     print(' ')
+
+# enumerate
+# jawa = [1, 'hitam', 0.5, False]
+# for index, wujud in enumerate(jawa):
+#     print(wujud)
+
+# looping while
+# jawab = 'ya'
+# hitung = 0
+# while(jawab == 'ya'):
+#     hitung += 1
+#     jawab = input("Ulang lagi? ")
+# print(f"Total perulangan: {hitung}")
+
+# bereakpoint
+# angka = [2, 5, 8, 12, 15, 7, 20]
+
+# print("Mencari angka pertama yang lebih besar dari 10...")
+# for n in angka:
+#     print(f"Sekarang memeriksa angka: {n}")
+#     if n > 10:
+#         print(f"Angka {n} lebih besar dari 10, Perulangan berhenti.")
+#         break
+# print("Program selesai.")
+
+# comtinue
+# for i in range(1, 11):
+#     if i % 2 != 0:
+#         continue
+#     print(f"Angka genap ditemukan: {i}")
+# print("\nProgram selesai.")
