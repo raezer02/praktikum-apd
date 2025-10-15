2e877405c114fbafb6bb7dee72590d3b7430e2ba
rae <rasyadanafdhalf@gmail.com>
2025-10-15 11:42:42 +0800

    add_post_test_05

---
+++ kelas/pertemuan-05/pertemuan_05.py
@@ -0,0 +1,15 @@
+# kelas = ['jawa', 20, True, 0.5,['APD', 25]]
+# print(kelas[4] [1])
+
+
+# appened add 
+# variable = ["Data Science", "Robotics", "Multimedia", "Network"]
+# # add jawa
+# variable.append("JAWA")
+# print(variable)
+
+
+variable = ["Data Science", "Robotics", "Multimedia", "Network"]
+# change to jawa
+variable.insert(2,"JAWA")
+print(variable)
