71027ab2d827b7f1cca55a1211a33c6026834a7c
rae <rasyadanafdhalf@gmail.com>
2025-10-28 22:37:40 +0800

    Update pertmeuan_07#baru.py

---
+++ kelas/pertemuan-07/pertmeuan_07#baru.py
@@ -0,0 +1,29 @@
+# def nama_fungsi(parameter):
+# pernyataan
+
+# def perkenalan():
+#     print(“Halo, aku Nabil”)
+          
+# perkenalan()
+
+# def salam():
+#     print (“Halo, jawa”)
+# def kali():
+#     e = 2*2
+#     print(e)
+
+# salam()
+# kali()
+
+# def luas_persegi_panjang(panjang, lebar):
+#     luas = panjang * lebar
+#     print (“luas persegi panjang adalah “, luas)
+
+# luas_persegi_panjang(4, 5)
+
+# # lokal
+# def info():
+#     Nama = "Informatika"
+#     Mata_Kuliah = "Logika Informatika"
+
+# ignore more code new
